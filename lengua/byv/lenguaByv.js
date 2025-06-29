549065daebd6d7c394dc0134b44d8e66514cca26
Laura Orcera <lauraorcera28@gmail.com>
2025-06-29 11:10:33 +0200

    byv

--- lengua/byv/lenguaByv.js
+++ lengua/byv/lenguaByv.js
@@ -1,28 +1,34 @@
 
 const words = [
-    { word: 'a__ioneta', correct: 'V', img: 'imagenes/avioneta.jpg' },
+
+    { word: 'tam__or', correct: 'B', img: 'imagenes/tambor.jpg' },
     { word: 'bam__u', correct: 'B', img: 'imagenes/1b-bambu.jpg' },
-    { word: 'bom__a', correct: 'B', img: 'imagenes/1b-bomba.jpg' },
-    { word: '__olcan', correct: 'V', img: 'imagenes/volcan.jpg' },
     { word: 'fram__uesa', correct: 'B', img: 'imagenes/1b-frambuesa.jpg' },
-    { word: 'bom__illa', correct: 'B', img: 'imagenes/1b-bombilla.jpg' },
-    { word: 'tam__or', correct: 'V', img: 'imagenes/tambor.jpg' },
-    { word: 'som__ra', correct: 'B', img: 'imagenes/1y3b-sombra.jpg' },
-    { word: 'som__rero', correct: 'B', img: 'imagenes/1y3b-sombrero.jpg' },
+    { word: 'em__udo', correct: 'B', img: 'imagenes/embudo.jpg' },
+    { word: 'com__ate', correct: 'B', img: 'imagenes/combate.jpg' },
+    { word: '__ampiro', correct: 'V', img: 'imagenes/vampiro.jpg' },
+    { word: 'ad__iento', correct: 'V', img: 'imagenes/adviento.jpg' },
+    { word: 'ad__ertencia', correct: 'V', img: 'imagenes/advertencia.jpg' },
+    { word: 'en__io', correct: 'V', img: 'imagenes/envio.jpg' },
+    { word: 'nu__lador', correct: 'B', img: 'imagenes/nublado.jpg' },
+
+
+    { word: 'a__ioneta', correct: 'V', img: 'imagenes/avioneta.jpg' },
+    { word: '__olcan', correct: 'V', img: 'imagenes/volcan.jpg' },
     { word: 'a__entura', correct: 'V', img: 'imagenes/aventurero.jpg' },
-    { word: 'her__ir', correct: 'V', img: 'imagenes/hervir.jpg' },
+    { word: '__ikingo', correct: 'V', img: 'imagenes/vikingo.jpg' },
+    { word: 'cla__o', correct: 'V', img: 'imagenes/clavo.jpg' },
+    { word: 'ol__idar', correct: 'V', img: 'imagenes/olvidar.jpg' },
+
+
+
+    { word: 'bom__illa', correct: 'B', img: 'imagenes/1b-bombilla.jpg' },
     { word: 'la__adora', correct: 'V', img: 'imagenes/lavadora.jpg' },
     { word: 'escri__ir', correct: 'B', img: 'imagenes/escribir.jpg' },
-    { word: '__ikingo', correct: 'V', img: 'imagenes/vikingo.jpg' },
-    { word: '__ampiro', correct: 'V', img: 'imagenes/vampiro.jpg' },
-    { word: '__erduras', correct: 'V', img: 'imagenes/verduras.jpg' },
     { word: 'hue__os', correct: 'V', img: 'imagenes/huevos.jpg' },
-    { word: 'cla__o', correct: 'V', img: 'imagenes/clavo.jpg' },
     { word: 'lla__ero', correct: 'V', img: 'imagenes/llavero.jpg' },
     { word: 'ca__ar', correct: 'V', img: 'imagenes/cavar.jpg' },
     { word: 'llu__ia', correct: 'V', img: 'imagenes/lluvia.jpg' },
-    { word: 'ol__idar', correct: 'V', img: 'imagenes/olvidar.jpg' },
-    { word: 'nu__lador', correct: 'B', img: 'imagenes/nublado.jpg' },
 
 ];
 
